9278ad8116e56ba4ebf74e66906c152873fde54a
jonim-git <n0majo01@students.oamk.fi>
2021-09-19 15:45:48 +0300

    simppeli lista

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const names = ['Jouni', 'Reima', 'Meija','Jouni']
+    setItems(names);
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: '30px' }}>
+      <h3>Names</h3>
+      <ul>
+        {items.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
